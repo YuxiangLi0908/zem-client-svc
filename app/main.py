867e59468de27ca9955e5c0aa1b7564c60aef7b2
Yuxiang LI <yuxiang.li@shipt.com>
2025-02-07 23:06:53 -0500

    initial commit

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Azure Container Apps!"}
+    return {"message": "Hello, Azure Container Registry and Container Apps!"}
