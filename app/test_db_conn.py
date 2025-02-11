907fafaaf523ceb255f32b047e03a5b71f27fbf5
Yuxiang LI <yuxiang.li@shipt.com>
2025-02-10 23:14:16 -0500

    initial comiit

--- app/test_db_conn.py
+++ app/test_db_conn.py
@@ -2,9 +2,9 @@ import os
 import psycopg2
 
 conn = psycopg2.connect(
-    user=os.environ.get("dbuser"),
-    password=os.environ.get("dbpass"),
-    host=os.environ.get("dbhost"),
-    port=int(os.environ.get("dbport")),
-    database=os.environ.get("dbname"),
+    user=os.environ.get("DBUSER"),
+    password=os.environ.get("DBPASS"),
+    host=os.environ.get("DBHOST"),
+    port=int(os.environ.get("DBPORT")),
+    database=os.environ.get("DBNAME"),
 )
