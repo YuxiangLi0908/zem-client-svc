9fbe39f00f6c119ffaf0f591de0690e712f573a6
Yuxiang LI <yuxiang.li@shipt.com>
2025-03-05 16:15:07 -0500

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import os
-import pandas as pd
 
-from sqlalchemy import text
+import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from sqlalchemy import text
 
+from app.api.router import api_router
 from app.test_db_conn import conn
-
 
 app = FastAPI()
 app.add_middleware(
@@ -15,13 +15,16 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 app.add_middleware(GZipMiddleware)
+app.include_router(api_router)
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, Azure Container Registry and Container Apps!"}
+
 
 @app.get("/dbconn")
 def read_root():
@@ -32,10 +35,15 @@ def read_root():
         "DBUSER": os.environ.get("DBUSER", ""),
     }
 
+
 @app.get("/dbtest")
 def read_db():
-    df = pd.read_sql("SELECT * FROM public.warehouse_vessel WHERE vessel_eta > '2025-01-02'", con=conn)
+    df = pd.read_sql(
+        "SELECT * FROM public.warehouse_vessel WHERE vessel_eta > '2025-01-02'",
+        con=conn,
+    )
     return {"tables": df.to_dict()}
+
 
 @app.get("/api/data")
 async def get_data():
--- app/test_db_conn.py
+++ app/test_db_conn.py
@@ -1,4 +1,5 @@
 import os
+
 import psycopg2
 
 conn = psycopg2.connect(
