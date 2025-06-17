66d64581e772bd17b99dcf4cc505d7b32f179a42
Yuxiang LI <yuxiang.li@shipt.com>
2025-06-17 00:40:10 -0400

    add shipment tracking

--- app/services/order_history.py
+++ app/services/order_history.py
@@ -1,12 +1,19 @@
 import pytz
-
 from fastapi import HTTPException
+from sqlalchemy import Numeric, cast, distinct, func
 from sqlalchemy.orm import Session, joinedload
 
 from app.data_models.db.container import Container
 from app.data_models.db.order import Order
+from app.data_models.db.pallet import Pallet
+from app.data_models.db.shipment import Shipment
 from app.data_models.db.user import User
-from app.data_models.order_tracking import OrderPreportResponse, OrderResponse, OrderPostportResponse
+from app.data_models.order_tracking import (
+    OrderPostportResponse,
+    OrderPreportResponse,
+    OrderResponse,
+    PalletShipmentSummary,
+)
 
 
 class OrderTracking:
@@ -42,65 +49,162 @@ class OrderTracking:
 
         if not order_data:
             raise HTTPException(
-                status_code=404, detail=f"No matching order found for {self.container_number}"
+                status_code=404,
+                detail=f"No matching order found for {self.container_number}",
             )
-        
+
         order_data = OrderPreportResponse.model_validate(order_data).model_dump()
         preport_history = []
-        china_tz = pytz.timezone('Asia/Shanghai')
+        china_tz = pytz.timezone("Asia/Shanghai")
+        pod = None
         if order_data["created_at"]:
-            preport_history.append({
-                "status": "ORDER_CREATED",
-                "description": f"创建订单: {order_data['container']['container_number']}",
-                "timestamp": order_data["created_at"].astimezone(china_tz),
-            })
+            preport_history.append(
+                {
+                    "status": "ORDER_CREATED",
+                    "description": f"创建订单: {order_data['container']['container_number']}",
+                    "timestamp": order_data["created_at"].astimezone(china_tz),
+                }
+            )
         if order_data["add_to_t49"]:
+            pod = order_data["vessel"]["destination_port"]
             if order_data["retrieval"]["temp_t49_pod_arrive_at"]:
-                preport_history.append({
-                    "status": "IN_TRANSIT",
-                    "description": f"到达港口: {order_data['vessel']['destination_port']}",
-                    "location": order_data["vessel"]["destination_port"],
-                    "timestamp": order_data["retrieval"]["temp_t49_pod_arrive_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "IN_TRANSIT",
+                        "description": f"到达港口: {order_data['vessel']['destination_port']}",
+                        "location": order_data["vessel"]["destination_port"],
+                        "timestamp": order_data["retrieval"][
+                            "temp_t49_pod_arrive_at"
+                        ].astimezone(china_tz),
+                    }
+                )
             if order_data["retrieval"]["temp_t49_pod_discharge_at"]:
-                preport_history.append({
-                    "status": "IN_TRANSIT",
-                    "description": f"港口卸货",
-                    "location": order_data["vessel"]["destination_port"],
-                    "timestamp": order_data["retrieval"]["temp_t49_pod_discharge_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "IN_TRANSIT",
+                        "description": f"港口卸货",
+                        "location": order_data["vessel"]["destination_port"],
+                        "timestamp": order_data["retrieval"][
+                            "temp_t49_pod_discharge_at"
+                        ].astimezone(china_tz),
+                    }
+                )
         if order_data["retrieval"]:
             if order_data["retrieval"]["scheduled_at"]:
-                preport_history.append({
-                    "status": "IN_TRANSIT",
-                    "description": f"预约港口提柜: 预计提柜时间 {order_data['retrieval']['target_retrieval_timestamp'].astimezone(china_tz)}",
-                    "location": order_data["vessel"]["destination_port"],
-                    "timestamp": order_data["retrieval"]["scheduled_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "IN_TRANSIT",
+                        "description": f"预约港口提柜: 预计提柜时间 {order_data['retrieval']['target_retrieval_timestamp'].astimezone(china_tz)}",
+                        "location": pod,
+                        "timestamp": order_data["retrieval"]["scheduled_at"].astimezone(
+                            china_tz
+                        ),
+                    }
+                )
             if order_data["retrieval"]["arrive_at_destination"]:
-                preport_history.append({
-                    "status": "ARRIVE_AT_WAREHOUSE",
-                    "description": f"港口提柜完成, 货柜到达目的仓点 {order_data['retrieval']['retrieval_destination_precise']}",
-                    "location": order_data["retrieval"]["retrieval_destination_precise"],
-                    "timestamp": order_data["retrieval"]["arrive_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "ARRIVE_AT_WAREHOUSE",
+                        "description": f"港口提柜完成, 货柜到达目的仓点 {order_data['retrieval']['retrieval_destination_precise']}",
+                        "location": order_data["retrieval"][
+                            "retrieval_destination_precise"
+                        ],
+                        "timestamp": order_data["retrieval"]["arrive_at"].astimezone(
+                            china_tz
+                        ),
+                    }
+                )
         if order_data["offload"]:
             if order_data["offload"]["offload_at"]:
-                preport_history.append({
-                    "status": "OFFLOAD",
-                    "description": "拆柜完成",
-                    "location": order_data["retrieval"]["retrieval_destination_precise"],
-                    "timestamp": order_data["offload"]["offload_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "OFFLOAD",
+                        "description": "拆柜完成",
+                        "location": order_data["retrieval"][
+                            "retrieval_destination_precise"
+                        ],
+                        "timestamp": order_data["offload"]["offload_at"].astimezone(
+                            china_tz
+                        ),
+                    }
+                )
             if order_data["retrieval"]["empty_returned"]:
-                preport_history.append({
-                    "status": "EMPTY_RETURN",
-                    "description": f"已归还空箱",
-                    "timestamp": order_data["retrieval"]["empty_returned_at"].astimezone(china_tz),
-                })
+                preport_history.append(
+                    {
+                        "status": "EMPTY_RETURN",
+                        "description": f"已归还空箱",
+                        "timestamp": order_data["retrieval"][
+                            "empty_returned_at"
+                        ].astimezone(china_tz),
+                    }
+                )
         order_data["history"] = preport_history
         return OrderPreportResponse.model_validate(order_data)
-    
+
     def _build_postport_history(self) -> OrderPostportResponse:
-        # TODO
-        return OrderPostportResponse(order_id="test-order-id")
+        try:
+            results = (
+                self.db_session.query(
+                    Pallet.destination,
+                    Pallet.PO_ID,
+                    Shipment.shipment_batch_number,
+                    Shipment.is_shipment_schduled,
+                    Shipment.shipment_schduled_at,
+                    Shipment.shipment_appointment,
+                    Shipment.is_shipped,
+                    Shipment.shipped_at,
+                    Shipment.is_arrived,
+                    Shipment.arrived_at,
+                    Shipment.pod_link,
+                    Shipment.pod_uploaded_at,
+                    func.round(cast(func.sum(Pallet.cbm), Numeric), 4).label("cbm"),
+                    func.round(
+                        cast(func.sum(Pallet.weight_lbs) / 2.20462, Numeric), 2
+                    ).label("weight_kg"),
+                    func.count(distinct(Pallet.id)).label("n_pallet"),
+                )
+                .join(Pallet.container)
+                .outerjoin(Pallet.shipment)
+                .filter(Container.container_number == self.container_number)
+                .group_by(
+                    Pallet.destination,
+                    Pallet.PO_ID,
+                    Shipment.shipment_batch_number,
+                    Shipment.is_shipment_schduled,
+                    Shipment.shipment_schduled_at,
+                    Shipment.shipment_appointment,
+                    Shipment.is_shipped,
+                    Shipment.shipped_at,
+                    Shipment.is_arrived,
+                    Shipment.arrived_at,
+                    Shipment.pod_link,
+                    Shipment.pod_uploaded_at,
+                )
+                .all()
+            )
+        except:
+            raise HTTPException(
+                status_code=404,
+                detail=f"No shipment history for {self.container_number}",
+            )
+        data = [
+            PalletShipmentSummary(
+                destination=row[0],
+                PO_ID=row[1],
+                shipment_batch_number=row[2],
+                is_shipment_schduled=row[3],
+                shipment_schduled_at=row[4],
+                shipment_appointment=row[5],
+                is_shipped=row[6],
+                shipped_at=row[7],
+                is_arrived=row[8],
+                arrived_at=row[9],
+                pod_link=row[10],
+                pod_uploaded_at=row[11],
+                cbm=row[12],
+                weight_kg=row[13],
+                n_pallet=row[14],
+            )
+            for row in results
+        ]
+        return OrderPostportResponse(shipment=data)
