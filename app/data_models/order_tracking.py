0f946cbaab2409c3f8e5df8758413c2b702433ca
sangwei <1178424577@qq.com>
2026-05-22 09:36:39 +0800

    1

--- app/data_models/order_tracking.py
+++ app/data_models/order_tracking.py
@@ -54,6 +54,7 @@ class PalletShipmentSummary(BaseModel):
     pod_uploaded_at: Optional[datetime] = None
     shipping_order_link: Optional[str] = None
     appointment_id: Optional[str] = None
+    shipment_type: Optional[str] = None
     cbm: Optional[float] = None
     weight_kg: Optional[float] = None
     n_pallet: Optional[float] = None
