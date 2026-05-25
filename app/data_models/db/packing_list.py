6ae71095c8a863691802ac115e47120948583180
sangwei <1178424577@qq.com>
2026-05-25 15:38:46 +0800

    1

--- app/data_models/db/packing_list.py
+++ app/data_models/db/packing_list.py
@@ -11,6 +11,7 @@ class PackingList(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     container_number_id = Column(Integer, ForeignKey("warehouse_container.id"), nullable=True)
+    shipment_batch_number_id = Column(Integer, ForeignKey("warehouse_shipment.id"), nullable=True)
     master_shipment_batch_number_id = Column(Integer, ForeignKey("warehouse_shipment.id"), nullable=True)
     quote_id_id = Column(Integer, nullable=True)
 
@@ -60,4 +61,8 @@ class PackingList(Base):
     ltl_address_type = Column(String(100), nullable=True)
 
     container = relationship("Container", backref="packing_list")
-    shipment = relationship("Shipment", backref="packing_list")
+    shipment = relationship(
+        "Shipment",
+        foreign_keys=[master_shipment_batch_number_id],
+        backref="packinglists_master",
+    )
